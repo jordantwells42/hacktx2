0d978b66602b1d1ef86f89ef594d369d95117f67
Axkita01 <94666562+Axkita01@users.noreply.github.com>
2022-10-15 15:04:49 -0500

    maps

--- frontend/src/pages/MapPage.jsx
+++ frontend/src/pages/MapPage.jsx
@@ -1,51 +1,26 @@
 import React from 'react'
 import { Map, Marker } from 'pigeon-maps'
 import axios from 'axios'
-import { useEffect, UseState } from 'react'
 
-function MapComp () {
-    const x = 30.30228
-    const y = -97.71958
-    const min_x = x - 5
-    const max_x = x + 5
-    const min_y = y - 5
-    const max_y = y + 5
-    const [c, setCenter] = React.useState([x, y])
-    const [data, setData] = React.useState([])
-    const [loading, setLoading] = React.useState(true)
-    const [error, setError] = React.useState(false)
+
+export default function MapComp () {
     const [markers, setMarkers] = React.useState([])
-
-    function tiler(x, y, z, dpr) {
-        return `https://a.tile.openstreetmap.fr/hot/${z}/${x}/${y}.png`;
-      }
-
-    /**useEffect(async () => {
-        axios.get('http://localhost:3001/api/locations')
-            .then(res => {
-                setData(res.data)
-                setLoading(false)
-            })
-            .catch(err => {
-                setError(err)
-                setLoading(false)
-            })
-    },[])*/
+    
+    /*React.useEffect(() => {
+        axios.get('http://localhost:3001/api/markers')
+        .then(res => {
+            setMarkers(res.data)
+        })
+    }, [])*/
+    
     return (
-        <Map 
-        minZoom = {16}
-        defaultZoom = {16}
-        maxZoom = {17}
-        center={c} 
-        >
+        <Map center={[51.505, -0.09]} zoom={9} width={600} height={400}>
+        {markers.map(marker => (
+            <Marker key={marker.id} anchor={[marker.lat, marker.lng]} payload={1} onClick={({ event, anchor, payload }) => {}} />
+        ))}
         </Map>
     )
-}
+    
 
-export default function MapPage () {
-    return (
-        <div className='h-screen overflow-x-hidden'>
-            <MapComp />
-        </div>
-    )
 }
+    
