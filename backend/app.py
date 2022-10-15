ff61de0dad235d954d5114b48c8360b0b08a9fa2
Michael Zhan <michael.zhan@utexas.edu>
2022-10-15 13:57:45 -0500

    done for now?

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,6 @@
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 
-from sqlalchemy import create_engine
-from sqlalchemy.pool import StaticPool
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.sqlite3'
@@ -12,10 +10,12 @@ db.init_app(app)
 class Locations(db.Model):
     location = db.Column(db.String, primary_key=True)
     image = db.Column(db.String)
+    x = db.Column(db.Float)
+    y = db.Column(db.Float)
     rating = db.Column(db.Float)
     count = db.Column(db.Integer)
     category = db.Column(db.String)
-    comments = db.Column(db.ARRAY(db.String))
+    comments = db.Column(db.JSON)
 
 with app.app_context():
     db.create_all()
@@ -34,13 +34,14 @@ def location():
     if request.method == 'POST':
         location = request.args.get('location')
         image = request.args.get('image')
+        x = request.args.get('x')
+        y = request.args.get('y')
         rating = request.args.get('rating')
         count = request.args.get('count')
         category = request.args.get('category')
         comments = request.args.get('comments')
-        new_location = Locations(location=location, image=image, rating=rating, count=count, category=category, comments=comments)
+        new_location = Locations(location=location, image=image, x=x, y=y, rating=rating, count=count, category=category, comments=comments)
 
         db.session.add(new_location)
         db.session.commit()
         return "Location added"
-
