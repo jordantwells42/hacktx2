d87877c56fb0bb53ff8bd5516256d0c5604d621f
Michael Zhan <michael.zhan@utexas.edu>
2022-10-15 12:18:38 -0500

    create blank table

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+db.init_app(app)
+
+class Locations(db.Model):
+    name = db.Column(db.String, primary_key=True)
+    image = db.Column(db.String)
+    rating = db.Column(db.Float)
+    count = db.Column(db.Integer)
+    category = db.Column(db.String)
+
+with app.app_context():
+    db.create_all()
