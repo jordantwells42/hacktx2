05432d04ab5faafc2c1435068242dd979d582945
Michael Zhan <michael.zhan@utexas.edu>
2022-10-15 13:36:58 -0500

    dummy tables

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,46 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
-app = Flask(__name__)
+from sqlalchemy import create_engine
+from sqlalchemy.pool import StaticPool
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.sqlite3'
+db = SQLAlchemy(app)
 db.init_app(app)
 
 class Locations(db.Model):
-    name = db.Column(db.String, primary_key=True)
+    location = db.Column(db.String, primary_key=True)
     image = db.Column(db.String)
     rating = db.Column(db.Float)
     count = db.Column(db.Integer)
     category = db.Column(db.String)
+    comments = db.Column(db.ARRAY(db.String))
 
 with app.app_context():
     db.create_all()
+
+@app.route("/")
+def show_all():
+   return Locations.query.all()
+
+@app.route('/location')
+def location():
+    if request.method == 'GET':
+        location = request.args.get('location')
+        location = Locations.query.filter_by(location=location).first()
+        return location
+
+    if request.method == 'POST':
+        location = request.args.get('location')
+        image = request.args.get('image')
+        rating = request.args.get('rating')
+        count = request.args.get('count')
+        category = request.args.get('category')
+        comments = request.args.get('comments')
+        new_location = Locations(location=location, image=image, rating=rating, count=count, category=category, comments=comments)
+
+        db.session.add(new_location)
+        db.session.commit()
+        return "Location added"
+
