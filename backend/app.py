849ff66ec4b3cd03d78e99f352fcd8f3605e0f93
Axkita01 <94666562+Axkita01@users.noreply.github.com>
2022-10-15 16:13:59 -0500

    get request fix

--- backend/app.py
+++ backend/app.py
@@ -21,6 +21,29 @@ class Locations(db.Model):
     category = db.Column(db.String)
     comments = db.Column(db.JSON)
 
+    def __init__(self, location, image, x, y, total_rating, count, category, comments):
+        self.location = location
+        self.image = image
+        self.x = x
+        self.y = y
+        self.total_rating = total_rating
+        self.count = count
+        self.category = category
+        self.comments = comments
+
+    @property
+    def serialize(self):
+        return {
+            'location': self.location,
+            'image': self.image,
+            'x': self.x,
+            'y': self.y,
+            'total_rating': self.total_rating,
+            'count': self.count,
+            'category': self.category,
+            'comments': self.comments
+        }
+
 with app.app_context():
     db.create_all()
 
@@ -32,12 +55,9 @@ def show_all():
 @cross_origin(supports_credentials=True)
 def location():
     if request.method == 'GET':
-        # Get all locations, FIXME: This is broken
-        r = request.data
-        print(r)
         location = request.args.get('location')
         location = Locations.query.filter_by(location=location).first()
-        return jsonify(location)
+        return jsonify(location.serialize)
 
     if request.method == 'POST':
         r = request.get_json()
